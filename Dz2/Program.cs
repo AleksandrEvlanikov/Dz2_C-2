56d972f95d766ef1b585b45a6cf36ea2620618b3
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-05 14:26:14 +0300

    Dz2

---
+++ Dz2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int number1 = Convert.ToInt32(Console.ReadLine() );
+String number1Text = Convert.ToString(number1);
+
+if(number1Text.Length > 2)
+{
+Console.WriteLine("третья цифра = " + number1Text[2]);
+}
+else
+{
+    Console.WriteLine("Нету 3 цифры " );
+}
+
