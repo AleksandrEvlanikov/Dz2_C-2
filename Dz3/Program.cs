1a6f1512737a7c2be2ba6f039de63bf8a6076f83
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-05 14:25:08 +0300

    Dz3

---
+++ Dz3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введи число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int a = 7;
+int b = 23;
+
+if (number1 % a == 0 && number1 % b == 0) 
+
+Console.WriteLine("Yes" );
+
+else
+{
+    Console.WriteLine("No" );
+}
